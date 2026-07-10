6115877779c93eb40fa79fc6c4f6d272929a2518
kjpayas <kvnpayas@gmail.com>
2026-07-10 17:36:12 +0800

    remove calculator

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -80,12 +80,12 @@
                   'icon' =>
                       'M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15',
               ],
-              [
-                  'label' => 'Calculator',
-                  'route' => 'customer.calculator',
-                  'icon' =>
-                      'M9 7h6m0 10v-3m-3 3h.01M9 17h.01M9 14h.01M12 14h.01M15 11h.01M12 11h.01M9 11h.01M7 21h10a2 2 0 002-2V5a2 2 0 00-2-2H7a2 2 0 00-2 2v14a2 2 0 002 2z',
-              ],
+              // [
+              //     'label' => 'Calculator',
+              //     'route' => 'customer.calculator',
+              //     'icon' =>
+              //         'M9 7h6m0 10v-3m-3 3h.01M9 17h.01M9 14h.01M12 14h.01M15 11h.01M12 11h.01M9 11h.01M7 21h10a2 2 0 002-2V5a2 2 0 00-2-2H7a2 2 0 00-2 2v14a2 2 0 002 2z',
+              // ],
               [
                   'label' => 'Business Centers',
                   'route' => 'customer.business-centers',
